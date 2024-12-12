aa62fac20ebe8c8f0e479c76ae350a6692d78e61
Emerson Lopes <emersonphilopes@gmail.com>
2024-12-11 22:26:09 -0300

    Add processing of files.

--- processing/process_files.py
+++ processing/process_files.py
@@ -1,21 +1,50 @@
 """Processing of documents text."""
 
+import json
 
-def process_file(file_path: str) -> tuple[str, str, str]:
-    """Process a single file.
+from pypdf import PdfReader
 
+
+def process_file(paper_path: str) -> tuple[str, str, str]:
+    """Process a single paper.
+
+    Expects a directory containing `abstract.json` and `article.pdf`
     Returns:
         A tuple containing:
             - The paper name;
             - The abstract text;
             - And the document main text (raw).
     """
-    raise NotImplementedError
+    file_text = process_paper_text(f"{paper_path}/article.pdf")
+    abstract, name = process_paper_metadate(f"{paper_path}/abstract.json")
+
+    return name, abstract, file_text
 
 
-def process_paper_text(text: str) -> str:
+def process_paper_text(path: str) -> str:
     """Return the processed text.
 
     This removes all unwanted content from the main text.
     """
-    raise NotImplementedError
+    file_text = ""
+
+    reader = PdfReader(path)
+    for i, page in enumerate(reader.pages):
+        txt = page.extract_text(0)
+        clean_text = txt.replace("\n", " ")
+        clean_text = clean_text.replace("- ", "")
+        abstract_index = clean_text.find("abstract")
+        if abstract_index != -1 and i <= 1:
+            clean_text = clean_text[abstract_index:]
+
+        file_text += clean_text
+
+    return file_text
+
+
+def process_paper_metadate(path: str) -> tuple[str, str]:
+    """Return the abstract and title of a paper."""
+    with open(path, "r", encoding="utf-8") as f:
+        info = json.load(f)
+
+    return info["Abstract"], info["Title"]
