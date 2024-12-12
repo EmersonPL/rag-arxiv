dd47daef67fc0e895b810882558f7a7a9b46da57
Emerson Lopes <emersonphilopes@gmail.com>
2024-12-11 22:26:40 -0300

    Allow optional parameters on DB connection utility.

--- scripts/migrations/utils.py
+++ scripts/migrations/utils.py
@@ -4,17 +4,17 @@ import psycopg2
 def connect_to_postgres(func):
     """Decorator to handle connector and cursor for functions that run on PG"""
 
-    def connection():
+    def connection(*args, **kwargs):
         conn = psycopg2.connect(
             database="rag_arxiv",
             user="admin",
             password="admin",
             host="localhost",
-            port=5432
+            port=5432,
         )
         cur = conn.cursor()
 
-        func(conn, cur)
+        func(conn, cur, *args, **kwargs)
 
         cur.close()
         conn.close()
