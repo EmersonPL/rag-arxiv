264e0518720f8a3017cdcbf2fe38041cf4f538e3
Emerson Lopes <emersonphilopes@gmail.com>
2024-12-15 01:09:36 -0300

    Update retrieval to load text from DB

--- rag_arxiv/retrieval/retrieve_papers.py
+++ rag_arxiv/retrieval/retrieve_papers.py
@@ -24,12 +24,24 @@ def retrieve_closest_neighbors(
     return results
 
 
-def retrieve_papers_text(papers_code: list[str]) -> list[str]:
+@connect_to_postgres
+def retrieve_papers_text(conn, cur, papers_code: list[str]) -> list[str]:
     """Return the text of the papers."""
     texts = []
     for paper in papers_code:
-        paper_path = f"{DATA_DIR}{paper}/article.pdf"
-        text = process_paper_text(path=paper_path)
+        cur.execute(
+            """
+            SELECT paper_text FROM papers WHERE code = %(paper)s
+            """,
+            {"paper": paper},
+        )
+
+        result = cur.fetchone()
+        if result is None:
+            paper_path = f"{DATA_DIR}{paper}/article.pdf"
+            text = process_paper_text(path=paper_path)
+        else:
+            text = result[0]
         texts.append(text)
 
     return texts
